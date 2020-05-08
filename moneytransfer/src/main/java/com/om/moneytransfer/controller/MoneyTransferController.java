1b071c6458db997cef7c835751b27d8929d6e8cb
omprakash <omprakash@DESKTOP-A1LBKMO.fios-router.home>
2020-05-08 06:37:45 -0400

    Retry logic

--- moneytransfer/src/main/java/com/om/moneytransfer/controller/MoneyTransferController.java
+++ moneytransfer/src/main/java/com/om/moneytransfer/controller/MoneyTransferController.java
@@ -1,5 +1,9 @@
 package com.om.moneytransfer.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +17,16 @@ import com.om.moneytransfer.model.MoneyExchange;
 public class MoneyTransferController {
 
 	RestTemplate restTemplate = new RestTemplate();
+	Logger logger = LoggerFactory.getLogger(MoneyTransferController.class);
 	
 	@RequestMapping("/moneytransfer")
+	@Retryable(value=RuntimeException.class,maxAttempts=5, backoff=@Backoff(delay=60000))
 	public MoneyExchange calculateExchg(@RequestParam(value="TotalAmount") Double amount,
 										@RequestParam(value="FromCurre") String fromCurr,
 										@RequestParam(value="ToCurr", defaultValue="USD") String toCurr){
 		
 		
+		logger.info("API call for amount:" + amount + "from currency:" + fromCurr + "to currency:" + toCurr);
 		ExchangeRate exchangeRate =  restTemplate.getForObject("http://localhost:8087/getxchg?FROM_CURRENCY={fromCurr}&TO_CURRENCY={toCurr}", 
 																ExchangeRate.class,fromCurr,toCurr);
 		
